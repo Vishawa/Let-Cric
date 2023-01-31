ce8628c6ef06e00e1c649f6da85bb9463feb8035
Vishal Awasthi <85893361+Vishawa@users.noreply.github.com>
2023-01-31 16:37:50 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,190 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Yuvraj Singh scored his first one-day century against",
+    answer: "Bangladesh",
+    options: [
+     "Bangladesh",
+     "Sri Lanka",
+     "Australia",
+     "New Zealand"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which Indian batsman has scored double century in ODI cricket match",
+    answer: "Rohit Sharma",
+    options: [
+     "Virat Kohli",
+     "Rohit Sharma",
+     "Kl Rahul",
+     "Suresh Raina"
+    ]
+  },
+    {
+    numb: 3,
+    question: "India won T20I world cup in which year",
+    answer: "2007",
+    options: [
+     "2008",
+     "2016",
+     "2007",
+     "2011"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Who is the fastest Indian bowler to reach 100 wickets in One-day internationals.",
+    answer: "Ajit Agarkar",
+    options: [
+     "Kapil dev",
+     "Anil Kumble",
+     "Ajit Agarkar",
+     "Zaheer Khan"
+    ]
+  },
+    {
+    numb: 5,
+    question: "How many players in the ground in cricket at a time",
+    answer: "11",
+    options: [
+      "11",
+      "12",
+      "10",
+      "15"
+    ]
+  },
+    {
+    numb: 6,
+    question: " What is color of ball used in limited over match ?",
+    answer: "White",
+    options: [
+      "Green",
+      "White",
+      "Red",
+      "Pink"
+    ]
+  },
+    {
+    numb: 7,
+    question: " Who among the following introduced the world to One-Day Internationals?",
+    answer: "Kerry Packer",
+    options: [
+     "Kerry Packer",
+     "Len Hutton",
+     "Frank Worrell",
+     "Thomas Hughes"
+    ]
+  },
+    {
+    numb: 8,
+    question: "Who was the captian of Indian Cricket team when India has won ODI cricket world cup",
+    answer: "MS Dhoni",
+    options: [
+     "Virat Kohli",
+     "Rohit Sharma",
+     "MS Dhoni",
+     "Rahul Dravid"
+    ]
+  },
+    {
+    numb: 9,
+    question: "International cricket is not played using which of the following balls",
+    answer: "Blue",
+    options: [
+      "White ball",
+      "Red ball",
+      "Pink ball",
+      "Blue"
+    ]
+  },
+    {
+    numb: 10,
+    question: "Which of the following is the oldest test cricket playing country in the world",
+    answer: "Australia",
+    options: [
+     "Australia",
+     "India",
+     "West Indies",
+     "South Africa"
+    ]
+  },
+    {
+    numb: 11,
+    question: "Where is the headquarter of the ICC?",
+    answer: "Dubai",
+    options: [
+      "London",
+      "Cape Town",
+      "Sydney",
+      "Dubai"
+    ]
+  },
+    {
+    numb: 12,
+    question: "Which of the following international competition is not organized by the ICC",
+    answer: "Syed Mushtaq Ali Trophy",
+    options: [
+      "Champions Trophy",
+      "World Cricket League",
+      " Under-19 Cricket World Cup",
+      "Syed Mushtaq Ali Trophy"
+    ]
+  },
+    {
+    numb: 13,
+    question: "Who is chosen at the ICC men's cricketer of the year 2017",
+    answer: "Virat Kohli",
+    options: [
+      "K.L. Rahul",
+      "Virat Kohli",
+      "Steve Smith",
+      "Rohit Sharma"
+    ]
+  },
+    {
+    numb: 14,
+    question: " When was the first Cricket World Cup held?",
+    answer: "1975",
+    options: [
+      "1975",
+      "1957",
+      "1987",
+      "1929"
+    ]
+  },
+  {
+    numb: 15,
+    question: "Which nation has won 2022 T20I World Title",
+    answer: "England",
+    options: [
+     "India",
+     "England",
+     "South Africa",
+     "Australia",
+    ]
+  },
+
+  
+
+
+  
+
+  
+
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
